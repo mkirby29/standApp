958bfe6d59768f3cab08afabdae49a471d514a87
paulcysu <su.paul.cy@gmail.com>
2018-08-20 15:50:03 -0700

    comment player failed - do to manual create

--- src/components/comment_player.js
+++ src/components/comment_player.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import '../assets/css/player.css';
 import Audio from 'react-audioplayer';
-import playlist from './playlist';
+// import songs from './playlist';
+import song from '../assets/audio/betterdays.mp3';
 
 // need to fix here, not audio_info!
 
@@ -10,12 +11,18 @@ class CommentPlayer extends Component {
         super(props)
     }
     render() {
+        // console.log('PLAYLIST:', songs); 
         return (
             <Audio
-                width={600}
-                height={400}
-                autoPlay={true}
-                playlist={playlist}
+                // width={600}
+                // height={400}
+                // autoPlay={true}
+                playlist={[
+                    {
+                        src: song,
+                        name: 'The song that is playing'
+                    }
+                ]}
             />
         )
     }
