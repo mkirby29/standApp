0c5c70c7c1e69286a6cc12f62e1a2d46effdec30
kyoung3212 <kevin.young3212@gmail.com>
2018-08-20 15:50:04 -0700

    avatar select skeleton

--- src/components/home.js
+++ src/components/home.js
@@ -3,13 +3,11 @@ import React, {Component} from 'react';
 import Header from './header';
 import NewsFeed from './newsFeed';
 import Footer from './footer';
-// import Avatar from './avatar_select'
 
 class Home extends Component {
     render () {
         return(
             <div>
-                {/* <Avatar/> */}
                 <Header/>
                 <NewsFeed/>
                 <Footer/>
