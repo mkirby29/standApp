7c3081d7c064dc200da9b124c1aa601597de7859
mikeyim337 <mk337337@gmail.com>
2018-08-20 19:00:11 -0700

    almost done with endpoints

---
+++ public/stand_app.php
@@ -0,0 +1,39 @@
+<?php
+$output = [
+    'success' => false
+];
+
+require_once('db_connect.php');
+
+$method = $_SERVER['REQUEST_METHOD'];
+$action = $_GET['action'];
+
+switch($method) {
+    case 'GET': 
+        require_once('get/'.$action.'.php');
+        break;
+    case 'POST':
+        require_once('post/'.$action.'.php');
+        break;
+    case 'PUT':
+        $output['message'] = 'PUT request made';
+        break;
+    case 'PATCH':
+        $_PATCH = json_decode(file_get_contents('php://input'), true);
+        require_once('patch/'.$action.'.php');
+        break;
+    case 'DELETE':
+        $output['message'] = 'DELETE request made';
+        break;
+    default:
+        $output['Error'] = 'Unknown request made';
+
+}
+
+$output = json_encode($output);
+
+print $output;
+
+
+
+?>
