fca7254f936ccd40a9789af261635217f3dfbe63
kyoung3212 <kevin.young3212@gmail.com>
2018-08-21 19:44:20 -0700

    removed comments from carousel map

--- src/components/avatar_select.js
+++ src/components/avatar_select.js
@@ -19,31 +19,7 @@ class Avatar extends Component {
     }
 
     render() {
-        
-        // var avatarArray = {
-        //     data: [
-        //         {name: 'Dj', src: Dj, id: '001'},
-        //         {name: 'Tuba', src: Tuba, id: '002'},
-        //         {name: 'Piano', src: Piano, id: '003'},
-        //         {name: 'Planet', src: Planet, id: '004'},
-        //         {name: 'Rapper', src: Rapper, id: '005'},
-        //         {name: 'Reading', src: Reading, id: '006'},
-        //         {name: 'Soccer', src: Soccer, id: '007'},
-        //         {name: 'Jedi', src: Jedi, id: '008'},
-        //         {name: 'Celebrity', src: Celebrity, id: '009'},
-        //         {name: 'Sax', src: Sax, id: '010'},
-        //     ]};
-        // let feedbackArray = avatarArray.data.map(function(ele, index){
-        //     return(
-        //         <div className="row" key={index}>
-        //             <div className="col-sm-6">
-        //                 <Link to='/'>
-        //                     <img key = {index} src={ele.src} className="img-thumbnail avatar_container"/>
-        //                 </Link>
-        //             </div>
-        //         </div>
-        //     )
-        // });
+
 
         return (
 
@@ -56,76 +32,6 @@ class Avatar extends Component {
                 </div>
             </div>
 
-
-            // return (
-
-            //     <div className='container-fluid'>
-            //         <div>
-            //             <p className="display-2 text-center">Select Avatar</p>     
-            //         </div>
-            //         <div className='row'>
-            //             <div className="text-center col-6">
-            //                 {feedbackArray}
-            //             </div>
-            //             <div className="text-center col-6">
-            //                 {feedbackArray}
-            //             </div>
-
-            //         </div>
-
-            //     </div>
-
-
-
-    //         <div>
-    //         <div className="container-fluid">
-    //         <h1 className="display-2">Pick your Avatar</h1>     
-    //      </div>
-    //      <div className="container-fluid">
-    //      <div className="row">
-    //          <div className="col-sm-6">
-    //              <img src={Dj} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //          <div className="col-sm-6">
-    //              <img src={Tuba} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //         </div> 
-    //      <div className="row">
-    //          <div className="col-sm-6">
-    //              <img src={Piano} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //          <div className="col-sm-6">
-    //              <img src={Planet} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //         </div> 
-    //      <div className="row">
-    //          <div className="col-sm-6">
-    //              <img src={Rapper} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //          <div className="col-sm-6">
-    //              <img src={Reading} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //         </div> 
-    //      <div className="row">
-    //          <div className="col-sm-6">
-    //              <img src={Soccer} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //          <div className="col-sm-6">
-    //              <img src={Jedi} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //         </div> 
-    //      <div className="row">
-    //          <div className="col-sm-6">
-    //              <img src={Celebrity} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //          <div className="col-sm-6">
-    //              <img src={Sax} alt="brettDj" className="img-thumbnail"/>
-    //          </div>
-    //         </div> 
-     
-    //         </div>
-  
-    //     </div>
         )
     }
 }
