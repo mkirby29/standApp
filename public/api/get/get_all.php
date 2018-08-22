eed24322e3e3f7472443caedf714075695268a9e
Matt Kirby <MattKir@aol.com>
2018-08-21 17:56:56 -0700

    stand_app.php modified-works

--- public/api/get/get_all.php
+++ public/api/get/get_all.php
@@ -1,5 +1,5 @@
 <?php
-print_r($_GET);
+// print_r($_GET);
 $query = 'SELECT u.user_id, audio_name, username, likes FROM users AS u JOIN audio AS a ON u.user_id = a.user_id JOIN user_feedback AS f ON f.audio_id = a.audio_id';
 
 $result = mysqli_query($conn, $query);
