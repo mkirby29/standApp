e6d6bc36427d1f19878baef126d18c5fb482fd4c
kyoung3212 <kevin.young3212@gmail.com>
2018-09-04 17:20:53 -0700

    completed DM box on profile post page

---
+++ src/components/dm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-widget';
+import avatar from '../assets/images/avatars/10kevinSoccer.jpg';
+
+ 
+import 'react-chat-widget/lib/styles.css';
+ 
+// import logo from './logo.svg';
+ 
+class Dm extends Component {
+  componentDidMount() {
+    addResponseMessage("Welcome to this awesome chat!");
+  }
+ 
+  handleNewUserMessage = (newMessage) => {
+    console.log(`New message incomig! ${newMessage}`);
+    // Now send the message throught the backend API
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar={avatar}
+          title="Direct Message"
+          subtitle="What did you think of my performance?"
+        />
+      </div>
+    );
+  } 
+}
+ 
+export default Dm;
