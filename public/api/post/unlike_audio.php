e66b709bc9d4d12c4997c3866ca75940ca24b3b8
paulcysu <su.paul.cy@gmail.com>
2018-09-10 15:41:55 -0700

    added php like templates

--- public/api/post/unlike_audio.php
+++ public/api/post/unlike_audio.php
@@ -1,6 +1,6 @@
 <?php
 
-// takes in audio ID
+// takes in audio ID and changes table...
 // { HEARTS } check if fill or not. and toggle accordingly
 // { AUDIO } get current likes counter and - 1
 
