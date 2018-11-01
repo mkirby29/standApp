aea97dfe05c010c295587ef86f76557c8477a211
paulcysu <su.paul.cy@gmail.com>
2018-10-31 17:01:27 -0700

    testing 2

--- public/api/post/add_audio.php
+++ public/api/post/add_audio.php
@@ -5,8 +5,8 @@ $name = $_POST['id'];
 $destination_path = getcwd().DIRECTORY_SEPARATOR;
 print($destination_path);
 
-// $target_file = $destination_path."post/sounds/audio.mp3";
-$target_file = $destination_path."./sounds/audio.mp3";
+$target_file = $destination_path."post/sounds/audio.mp3";
+// $target_file = $destination_path."./sounds/audio.mp3";
 
 move_uploaded_file($_FILES['audio']['tmp_name'], $target_file);
 
