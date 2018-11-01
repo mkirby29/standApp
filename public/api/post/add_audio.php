18d36a4e6e44fb46bbaa6f63b56e8994b9bd036b
paulcysu <su.paul.cy@gmail.com>
2018-10-31 19:32:30 -0700

    print commands

--- public/api/post/add_audio.php
+++ public/api/post/add_audio.php
@@ -4,11 +4,13 @@ $thing = $_FILES['audio'];
 $name = $_POST['id'];
 $destination_path = getcwd().DIRECTORY_SEPARATOR;
 print($destination_path);
+print('$thing: ' . $thing);
 
 $target_file = $destination_path."post/sounds/audio.mp3";
 // $target_file = $destination_path."./sounds/audio.mp3";
 
 move_uploaded_file($_FILES['audio']['tmp_name'], $target_file);
+print('target_file: ' . $target_file);
 
 require_once('get/store_audio.php');
 
@@ -23,9 +25,9 @@ if(!$author) {
 $audioName=$_POST['audio_name'];
 $user_id=$_POST['user_id'];
 
-print_r($author, $audioName, $user_id);
-// $query = "INSERT INTO `audio`(`author_name`, `audio_name`, `user_id`, `likes`) VALUES ('$author', '$audioName', '$user_id', 0)";
-$query = "INSERT INTO `audio`(`author_name`, `audio_name`, `user_id`, `likes`) VALUES ('Testing', '$audioName', '$user_id', 0)";
+print('authorname: ' . $author);
+$query = "INSERT INTO `audio`(`author_name`, `audio_name`, `user_id`, `likes`) VALUES ('$author', '$audioName', '$user_id', 0)";
+// $query = "INSERT INTO `audio`(`author_name`, `audio_name`, `user_id`, `likes`) VALUES ('Testing', '$audioName', '$user_id', 0)";
 
 // INSERT INTO `user_feedback` (`id`, `likes`, *`audio_id`*) VALUES (NULL, '0', ' *audio_id* ');
 
